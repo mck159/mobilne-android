c61e481e6ce0f276c19d32dc85ee1aa9ee51d855
Maciej Sroka <maciej.sroka2@gmail.com>
2016-06-04 15:31:03 +0200

    On/off buttons

--- app/src/main/java/srokawegrzyn/agh/edu/pl/bleperipheralimei/MainActivity.java
+++ app/src/main/java/srokawegrzyn/agh/edu/pl/bleperipheralimei/MainActivity.java
@@ -6,6 +6,7 @@ import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattServer;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothManager;
+import android.bluetooth.le.AdvertiseCallback;
 import android.bluetooth.le.AdvertiseData;
 import android.bluetooth.le.AdvertiseSettings;
 import android.bluetooth.le.BluetoothLeAdvertiser;
@@ -13,6 +14,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import java.util.UUID;
 
@@ -23,7 +25,14 @@ public class MainActivity extends AppCompatActivity {
             .fromString("0000180F-0000-1000-8000-00805f9b34f2");
     private static final UUID BATTERY_LEVEL_UUID = UUID
             .fromString("00002A19-0000-1000-8000-00805f9b34f3");
-
+    private BluetoothLeAdvertiser bleAdvertiser;
+    private AdvertiseSettings mAdvSettings;
+    private AdvertiseData mAdvData;
+    private AdvertiseCallback bleAdveriseCallback = new MyAdvertiseCallback();
+    private BluetoothAdapter bluetoothAdapter;
+    private BluetoothManager bluetoothManager;
+    private BluetoothGattService mBatteryService;
+    private BluetoothGattServer gattServer;
 
 
     @Override
@@ -31,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
 
         BluetoothGattCharacteristic mBatteryLevelCharacteristic = new BluetoothGattCharacteristic(BATTERY_LEVEL_UUID,
                 BluetoothGattCharacteristic.PROPERTY_READ | BluetoothGattCharacteristic.PROPERTY_NOTIFY,
@@ -40,31 +49,29 @@ public class MainActivity extends AppCompatActivity {
         mBatteryLevelCharacteristic.addDescriptor(
                 getClientCharacteristicConfigurationDescriptor());
 
-        BluetoothGattService mBatteryService = new BluetoothGattService(BATTERY_SERVICE_UUID,
+        mBatteryService = new BluetoothGattService(BATTERY_SERVICE_UUID,
                 BluetoothGattService.SERVICE_TYPE_PRIMARY);
         mBatteryService.addCharacteristic(mBatteryLevelCharacteristic);
 
 
-        BluetoothGattServer gattServer = bluetoothManager.openGattServer(this, new GattServerCallback());
-        gattServer.addService(mBatteryService);
 
-        BluetoothAdapter bluetoothAdapter = bluetoothManager.getAdapter();
-        BluetoothLeAdvertiser bleAdvert iser = bluetoothAdapter.getBluetoothLeAdvertiser();
 
-        AdvertiseSettings mAdvSettings = new AdvertiseSettings.Builder()
+        bluetoothAdapter = bluetoothManager.getAdapter();
+        bleAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
+
+        mAdvSettings = new AdvertiseSettings.Builder()
                 .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_BALANCED)
                 .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
                 .setConnectable(true)
                 .build();
-        AdvertiseData mAdvData = new AdvertiseData.Builder()
-                .setIncludeDeviceName(true)
+        mAdvData = new AdvertiseData.Builder()
+//                .setIncludeDeviceName(true)
                 .setIncludeTxPowerLevel(true)
                 .addServiceUuid(getServiceUUID())
                 .build();
 
 
         bleAdvertiser = bluetoothAdapter.getBluetoothLeAdvertiser();
-        bleAdvertiser.startAdvertising(mAdvSettings, mAdvData, new MyAdvertiseCallback());
     }
 
     public static BluetoothGattDescriptor getClientCharacteristicConfigurationDescriptor() {
@@ -73,6 +80,18 @@ public class MainActivity extends AppCompatActivity {
     }
     public ParcelUuid getServiceUUID() {
         return new ParcelUuid(BATTERY_SERVICE_UUID);
+    }
+
+    public void buttonStartAdvertisingClick(View v) {
+        bleAdvertiser.startAdvertising(mAdvSettings, mAdvData, bleAdveriseCallback);
+        gattServer = bluetoothManager.openGattServer(this, new GattServerCallback());
+        gattServer.addService(mBatteryService);
+
+    }
+
+    public void buttonStopAdvertisingClick(View v) {
+        bleAdvertiser.stopAdvertising(bleAdveriseCallback);
+        gattServer.close();
     }
 
     void writeToast(String msg) {
